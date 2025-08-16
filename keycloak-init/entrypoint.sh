41fddc7f7265cf354d30febb2f76c0e6cfed30d4
Mauricio Generoso <mauricio.generoso@hotmail.com>
2025-08-16 20:28:30 +0100

    closes #118: sets up default client secret on keycloak init

--- keycloak-init/entrypoint.sh
+++ keycloak-init/entrypoint.sh
@@ -7,6 +7,9 @@ users=("client" "support")
 passwords=("client-password" "support-password")
 roles=("client-user" "support-user")
 
+clients=("groot-mg-identity-app")
+secrets=("yeL37FpucV9slQGp6r5OTmzEu485yETv")
+
 function error_exit() {
   echo "Error: $1"
   exit 1
@@ -43,9 +46,26 @@ function create_users() {
     /opt/keycloak/bin/kcadm.sh create users/$userId/role-mappings/realm -r $REAL_NAME -b "[{\"id\":\"$roleId\",\"name\":\"$role\"}]" \
       || error_exit "Failed to assign role $role to user $username."
   done
+
+  echo "All users created successfully."
 }
 
+function set_client_secrets() {
+  for i in "${!clients[@]}"; do
+    clientId=${clients[$i]}
+    secretValue=${secrets[$i]}
+
+    echo "Setting client secret for $clientId"
+
+    clientUUID=$(/opt/keycloak/bin/kcadm.sh get clients -r $REAL_NAME -q clientId=$clientId --fields id --format csv | tail -n1 | tr -d '"') \
+      || error_exit "Failed to get ID for client $clientId."
+
+    /opt/keycloak/bin/kcadm.sh update clients/$clientUUID -r $REAL_NAME -s secret=$secretValue \
+      || error_exit "Failed to set secret for client $clientId."
+  done
+
+  echo "All clients updated successfully."
+}
 admin_login
 create_users
-
-echo "All users created successfully."
+set_client_secrets
